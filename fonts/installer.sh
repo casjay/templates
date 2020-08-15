551324a7476f3318db1d8a72f7f1efedfd0738f0
casjay <git-admin@casjaysdev.com>
2020-08-15 13:56:38 -0400

    Update installer.sh

--- fonts/installer.sh
+++ fonts/installer.sh
@@ -1,101 +1,191 @@
 #!/usr/bin/env bash
 
-APPNAME="DFAPPNAME"
-SCRIPTNAME="$(basename $0)"
-SCRIPTDIR="$(dirname "${BASH_SOURCE[0]}")"
+APPNAME="$(basename $0)"
+USER="${SUDO_USER:-${USER}}"
+HOME="${USER_HOME:-${HOME}}"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# @Author      : Jason
-# @Contact     : casjaysdev@casjay.net
-# @File        : install
-# @Created     : Mon, Dec 31, 2019, 00:00 EST
-# @License     : WTFPL
-# @Copyright   : Copyright (c) CasjaysDev
-# @Description : installer script for DFAPPNAME
+# @Author          : Jason
+# @Contact         : casjaysdev@casjay.net
+# @File            : install.sh
+# @Created         : Wed, Aug 09, 2020, 02:00 EST
+# @License         : WTFPL
+# @Copyright       : Copyright (c) CasjaysDev
+# @Description     : installer script for template font
 #
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Set functions
 
-if [ -f /usr/local/share/CasjaysDev/scripts/functions/app-installer.bash ]; then
-    . /usr/local/share/CasjaysDev/scripts/functions/app-installer.bash
-elif [ -f "$HOME/.local/share/scripts/functions/app-installer.bash" ]; then
-    . "$HOME/.local/share/scripts/functions/app-installer.bash"
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/casjay-dotfiles/scripts/raw/master/functions}"
+SCRIPTSFUNCTDIR="${SCRIPTSAPPFUNCTDIR:-/usr/local/share/CasjaysDev/scripts}"
+SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-app-installer.bash}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if [ -f "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE" ]; then
+  . "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE"
+elif [ -f "$HOME/.local/share/CasjaysDev/functions/$SCRIPTSFUNCTFILE" ]; then
+  . "$HOME/.local/share/CasjaysDev/functions/$SCRIPTSFUNCTFILE"
 else
-    mkdir -p "$HOME/.local/share/scripts/functions"
-    curl -LSs https://github.com/casjay-dotfiles/scripts/raw/master/functions/app-installer.bash -o "$HOME/.local/share/scripts/functions/app-installer.bash"
-    . "$HOME/.local/share/scripts/functions/app-installer.bash"
+  mkdir -p "/tmp/CasjaysDev/functions"
+  curl -LSs "$SCRIPTSFUNCTURL/$SCRIPTSFUNCTFILE" -o "/tmp/CasjaysDev/functions/$SCRIPTSFUNCTFILE" || exit 1
+  . "/tmp/CasjaysDev/functions/$SCRIPTSFUNCTFILE"
 fi
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Make sure the scripts repo is installed
+
+#scripts_check
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Defaults
 
-CONF="/usr/local/etc/CasjaysDev"
-FONTDIR="/usr/local/share/fonts"
-HOMEDIR="/usr/local/share/CasjaysDev/fontmgr"
-APPDIR="/usr/local/share/CasjaysDev/fontmgr/$APPNAME"
-REPO="${FONTMGRREPO:-https://github.com/fontmgr}"
-
-# Version
-APPVERSION="$(curl -LSs $REPO/$APPNAME/raw/master/version.txt)"
+APPNAME="template"
+PLUGNAME=""
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# Requires root
+# git repos
 
-sudoreq  # sudo required
+PLUGINREPO=""
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Version
+
+APPVERSION="$(curl -LSs ${FONTMGRREPO:-https://github.com/fontmgr}/$APPNAME/raw/master/version.txt)"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# options are: user_installdirs system_installdirs pkmgr_installer iconmgr_installer font_installer theme_installer
+
+font_installer
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Set options
+
+APPDIR="$SHARE/CasjaysDev/fontmgr/$APPNAME"
+PLUGDIR="$SHARE/$APPNAME/${PLUGNAME:-plugins}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Script options IE: --help
+
+show_optvars "$@"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Requires root - no point in continuing
+
+#sudoreq  # sudo required
 #sudorun  # sudo optional
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# end with a space
+
+APP=""
+PERL=""
+PYTH=""
+PIPS=""
+CPAN=""
+GEMS=""
+
+# install packages - useful for package that have the same name on all oses
+install_packages $APP
+
+# install required packages using file
+install_required $APP
+
+# check for perl modules and install using system package manager
+install_perl $PERL
+
+# check for python modules and install using system package manager
+install_python $PYTH
+
+# check for pip binaries and install using python package manager
+install_pip $PIPS
+
+# check for cpan binaries and install using perl package manager
+install_cpan $CPAN
+
+# check for ruby binaries and install using ruby package manager
+install_gem $GEMS
+
+# Other dependencies
+dotfilesreq
+dotfilesreqadmin
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Ensure directories exist
 
-mkd "$HOMEDIR"
-mkd "$FONTDIR"
-mkd "$CONF/CasjaysDev/fontmgr"
+ensure_dirs
+ensure_perms
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Main progam
 
 if [ -d "$APPDIR/.git" ]; then
-    execute \
-          "cd $APPDIR && \
-           git_update" \
-          "Updating $APPNAME font"
+  execute \
+    "git_update $APPDIR" \
+    "Updating $APPNAME fonts"
 else
-    if [ -d "$BACKUPDIR/$APPNAME" ]; then
-        rm_rf "$BACKUPDIR"/"$APPNAME"
-    fi
-    execute \
-          "mv_f $APPDIR $BACKUPDIR/$APPNAME && \
-           git_clone -q $REPO/$APPNAME $APPDIR" \
-          "Installing $APPNAME font"
+  execute \
+    "backupapp && \
+    git_clone -q $REPO/$APPNAME $APPDIR" \
+    "Installing $APPNAME fonts"
 fi
+
+# exit on fail
+failexitcode
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Plugins
+
+if [ "$PLUGNAME" != "" ]; then
+  if [ -d "$PLUGDIR"/.git ]; then
+    execute \
+      "git_update $PLUGDIR" \
+      "Updating $PLUGNAME"
+  else
+    execute \
+      "git_clone $PLUGINREPO $PLUGDIR" \
+      "Installing $PLUGNAME"
+  fi
+fi
+
+# exit on fail
+failexitcode
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # run post install scripts
 
-run_postinst() { ln_sf "$HOMEDIR/$APPNAME/fonts" "$FONTDIR/$APPNAME" ; devnull fc-cache -fv ;}
+run_postinst() {
+  run_postinst_global
 
-     execute \
-          "run_postinst" \
-          "Running post install scripts"
+}
+
+execute \
+  "run_postinst" \
+  "Running post install scripts"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # create version file
 
-if [ ! -f "$CONF/CasjaysDev/fontmgr/$APPNAME" ] && [ -f "$APPDIR/version.txt" ]; then
-    ln_sf "$APPDIR/version.txt" "$CONF/CasjaysDev/fontmgr/$APPNAME"
-fi
+install_fontmgr_version
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # exit
-if [ ! -z "$EXIT" ]; then exit "$EXIT"; fi
+run_exit
 
 # end
-#/* vi: set expandtab ts=4 noai
